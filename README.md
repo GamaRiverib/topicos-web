46821aa5022e76fb9784fca3a6749c31780b3533
José Gamaliel Rivera Ibarra <j0s3.r1v3r@gmail.com>
2021-09-22 20:30:25 -0700

    Se agregaron los métodos update y delete

---
+++ README.md
@@ -0,0 +1,2 @@
+# Topicos Web
+En este repositorio encontrarás el código fuente de algunos de los ejemplos vistos en las clases del curso Topicos Web
--- topicosweb-nosql/index.ts
+++ topicosweb-nosql/index.ts
@@ -1,5 +1,14 @@
-import { MongoClient, Db } from "mongodb";
+import { MongoClient, Db, ObjectId } from "mongodb";
 import { Command } from "commander";
+import { cyan, green, red, yellow } from "colors";
+import CliTable3, { TableConstructorOptions } from "cli-table3";
+
+
+interface Alumno {
+  name: string;
+  career: string;
+  age?: number;
+}
 
 async function getDatabase(): Promise<Db> {
   // Connection url
@@ -19,6 +28,23 @@ async function getDatabase(): Promise<Db> {
   }
 }
 
+function getDatabase2(): Promise<Db> {
+  return new Promise((resolve, reject) => {
+    // Connection url
+    const url = "mongodb://localhost:27017";
+    // Database Name
+    const dbName = "topicosweb";
+    // Connect using MongoClient
+    const mongoClient = new MongoClient(url);
+    mongoClient.connect()
+      .then((client: MongoClient) => {
+        const db = client.db(dbName);
+        resolve(db);
+      })
+      .catch(reject);
+  });
+}
+
 const program = new Command();
 program
   .version("0.1.0")
@@ -28,25 +54,32 @@ program
     try {
       const db: Db = await getDatabase();
       const collection = db.collection("alumnos");
-      const query: { name?: string } = {};
+      let query: any = {};
       if (name) {
-        query.name = name;
+        query = { name: { '$regex': name, '$options': "ig" } };
       }
       const result = collection.find(query);
       const arr = await result.toArray();
       console.log(arr);
+      const head: string[] = ["id", "name", "career", "age"];
+      const colWidths: number[] = [30, 30, 10, 10];
+      const options: TableConstructorOptions = { head, colWidths };
+      const table = new CliTable3(options);
+      arr.forEach(a => {
+        const row = [];
+        row.push({ content: a._id.toString() });
+        row.push({ content: a.name });
+        row.push({ content: a.career || "-" });
+        row.push({ content: a.age || "X" });
+        table.push(row);
+      });
+      console.log(table.toString());
       process.exit(0);
     } catch (err: any) {
-      console.log(err);
+      console.log(`${red('Error')}: ${yellow(err.toString())}`);
       process.exit(1);
     }
 });
-
-interface Alumno {
-  name: string;
-  career: string;
-  age?: number;
-}
 
 program
   .command("add <name>")
@@ -55,7 +88,6 @@ program
   .description("Permite agregar un alumno")
   .action(async (name: string, options: { career: string, age?: string }) => {
     try {
-      console.debug(options);
       const alumno: Alumno = {
         name,
         career: options.career,
@@ -64,12 +96,58 @@ program
       const db: Db = await getDatabase();
       const collection = db.collection("alumnos");
       const result = await collection.insertOne(alumno);
-      console.log(result);
+      console.log(green('Registro exitoso'));
+      console.log({ alumno: result });
       process.exit(0);
     } catch (err: any) {
-      console.log(err);
+      console.log(`${red('Error')}: ${yellow(err.toString())}`);
       process.exit(1);
     }
   });
+
+program
+  .command("update <id>")
+  .option("-n, --name <name>", "El nombre del alumno")
+  .option("-c, --career <career>", "La carrera que estudia el alumno")
+  .option("-a, --age <age>", "La edad actual del alumno")
+  .description("Actualiza la información de una alumno mediante su Id")
+  .action(async (id: string, alumno: { name: string, career: string, age: string }) => {
+    try {
+      const query = { _id: new ObjectId(id) };
+      const db: Db = await getDatabase();
+      const collection = db.collection("alumnos");
+      const result = await collection.updateOne(query, { $set: alumno });
+      if (result.modifiedCount == 0) {
+        console.log(`${yellow('Alerta:')} algo salió mal`);
+      } else {
+        console.log(`${green('Se actualizó exitosamente')}`);
+      }
+      process.exit(0);
+    } catch (err: any) {
+      console.log(`${red('Error')}: ${yellow(err.toString())}`);
+      process.exit(1);
+    }
+  });
+
+program
+  .command("delete <id>")
+  .description("Elimina la información de un alumno mediante su Id")
+  .action(async (id: string) => {
+    try {
+      const query = { _id: new ObjectId(id) };
+      const db = await getDatabase();
+      const collection = db.collection("alumnos");
+      const result = await collection.deleteOne(query);
+      if (result.deletedCount == 0) {
+        console.log(`${yellow('Alerta:')} algo salió mal`);
+      } else {
+        console.log(`${cyan('Se eliminó exitosamente')}`);
+      }
+      process.exit(0);
+    } catch (err: any) {
+      console.log(`${red('Error')}: ${yellow(err.toString())}`);
+      process.exit(1);
+    }
+  })
 
 program.parse(process.argv);
