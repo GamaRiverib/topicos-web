11c38da0e564cfe80422d45584e0b4f0e0758795
José Gamaliel Rivera Ibarra <j0s3.r1v3r@gmail.com>
2021-10-11 20:33:17 -0700

    Más ejemplo de uso de express

--- express-hola-mundo/index.js
+++ express-hola-mundo/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.use(express.static("./public"));
+
 app.get("/", (req, res, next) => {
   res.status(201).send("Hola mundo!");
 });
@@ -36,6 +40,19 @@ app.get("/hola", decirHola, sayHello, (req, res) => {
 app.get("/hola/:nombre", (req, res, next) => {
   const nombre = req.params.nombre;
   res.send(`Hola ${nombre}`);
+});
+
+app.post("/alumno", (req, res, next) => {
+  console.log(req.body);
+  console.log(req.query);
+  // Almacenar DB
+  const data = req.body;
+  data.id = Math.random() * 10;
+  res.json(data);
+});
+
+app.get("/ejemplo", (req, res, next) => {
+  res.redirect("/hola");
 });
 
 app.listen(3000, (err) => {
